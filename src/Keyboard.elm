e21cb5e01fe798cc496d53e35851a49cd7bdbafd
Justin Woo <moomoowoo@gmail.com>
2016-02-08 23:09:28 +0900

    Add keyCode signal for keydown events

--- src/Keyboard.elm
+++ src/Keyboard.elm
@@ -1,7 +1,7 @@
 module Keyboard
     ( arrows, wasd
     , enter, space, ctrl, shift, alt, meta
-    , isDown, keysDown, presses
+    , isDown, keysDown, presses, downs
     ) where
 
 {-| Library for working with keyboard input.
@@ -201,3 +201,7 @@ presses : Signal KeyCode
 presses =
   Signal.map .keyCode Native.Keyboard.presses
 
+{-| A Signal of keyCodes from the window 'keydown' event -}
+downs : Signal KeyCode
+downs =
+  Signal.map .keyCode Native.Keyboard.downs
